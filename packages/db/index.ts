dd4944d886315d8eb9eca560bd9416af7675ca8d
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-09 17:15:55 -0800

    Test

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -5,8 +5,6 @@ import * as post from "./schema/post";
 
 export const schema = { ...post };
 
-export { mySqlTable as tableCreator } from "./schema/_table";
-
 export * from "drizzle-orm";
 
 const queryClient = postgres(process.env.DATABASE_URL);
--- packages/db/schema/post.ts
+++ packages/db/schema/post.ts
@@ -1,14 +1,12 @@
-import { sql } from 'drizzle-orm';
-import { serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { sql } from "drizzle-orm";
+import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
-import { mySqlTable } from './_table';
-
-export const post = mySqlTable('post', {
-  id: serial('id').primaryKey(),
-  title: varchar('name', { length: 256 }).notNull(),
-  content: varchar('content', { length: 256 }).notNull(),
-  createdAt: timestamp('created_at')
+export const post = pgTable("post", {
+  id: serial("id").primaryKey(),
+  title: varchar("name", { length: 256 }).notNull(),
+  content: varchar("content", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
-  updatedAt: timestamp('updatedAt'),
+  updatedAt: timestamp("updatedAt"),
 });
