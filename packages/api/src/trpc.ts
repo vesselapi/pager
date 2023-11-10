577e1610d0a2ff596b6f78751f96715e5db4e2c6
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-09 17:39:02 -0800

    Fix

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -1,68 +1,117 @@
-{
-  "name": "@vessel/expo",
-  "version": "0.1.0",
-  "private": true,
-  "main": "expo-router/entry",
-  "scripts": {
-    "clean": "git clean -xdf .expo .turbo node_modules",
-    "dev:mobile": "expo start --ios",
-    "dev:android": "expo start --android",
-    "dev:ios": "expo start --ios",
-    "lint": "eslint .",
-    "format": "prettier --check \"**/*.{js,cjs,mjs,ts,tsx,md,json}\"",
-    "typecheck": "tsc --noEmit",
-    "android": "expo run:android",
-    "ios": "expo run:ios"
-  },
-  "dependencies": {
-    "@expo/metro-config": "^0.10.7",
-    "@shopify/flash-list": "1.4.3",
-    "@tanstack/react-query": "^5.8.1",
-    "@trpc/client": "next",
-    "@trpc/react-query": "next",
-    "@trpc/server": "next",
-    "expo": "^49.0.16",
-    "expo-constants": "~14.4.2",
-    "expo-linking": "~5.0.2",
-    "expo-router": "2.0.11",
-    "expo-splash-screen": "~0.22.0",
-    "expo-status-bar": "~1.7.1",
-    "nativewind": "^4.0.13",
-    "react": "18.2.0",
-    "react-dom": "18.2.0",
-    "react-native": "0.72.6",
-    "react-native-gesture-handler": "~2.12.0",
-    "react-native-reanimated": "~3.3.0",
-    "react-native-safe-area-context": "4.6.3",
-    "react-native-screens": "~3.22.1",
-    "superjson": "2.2.0"
-  },
-  "devDependencies": {
-    "@vessel/api": "workspace:^0.1.0",
-    "@vessel/eslint-config": "workspace:^0.2.0",
-    "@vessel/prettier-config": "workspace:^0.1.0",
-    "@vessel/tailwind-config": "workspace:^0.1.0",
-    "@vessel/tsconfig": "workspace:^0.1.0",
-    "@babel/core": "^7.23.2",
-    "@babel/preset-env": "^7.23.2",
-    "@babel/runtime": "^7.23.2",
-    "@expo/config-plugins": "^7.2.5",
-    "@types/babel__core": "^7.20.4",
-    "@types/react": "^18.2.37",
-    "eslint": "^8.53.0",
-    "prettier": "^3.0.3",
-    "tailwindcss": "3.3.5",
-    "typescript": "^5.2.2"
-  },
-  "eslintConfig": {
-    "root": true,
-    "extends": [
-      "@vessel/eslint-config/base",
-      "@vessel/eslint-config/react"
-    ],
-    "ignorePatterns": [
-      "expo-plugins/**"
-    ]
-  },
-  "prettier": "@vessel/prettier-config"
+/**
+ * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
+ * 1. You want to modify request context (see Part 1)
+ * 2. You want to create a new middleware or type of procedure (see Part 3)
+ *
+ * tl;dr - this is where all the tRPC server stuff is created and plugged in.
+ * The pieces you will need to use are documented accordingly near the end
+ */
+import { initTRPC } from "@trpc/server";
+import { db } from "@vessel/db";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API
+ *
+ * These allow you to access things like the database, the session, etc, when
+ * processing a request
+ *
+ */
+interface CreateContextOptions {
+  headers: Headers;
 }
+
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use
+ * it, you can export it from here
+ *
+ * Examples of things you may need it for:
+ * - testing, so we dont have to mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
+ */
+const createInnerTRPCContext = (opts: CreateContextOptions) => {
+  return {
+    headers: opts.headers,
+    db,
+  };
+};
+
+/**
+ * This is the actual context you'll use in your router. It will be used to
+ * process every request that goes through your tRPC endpoint
+ * @link https://trpc.io/docs/context
+ */
+export const createTRPCContext = async (opts: { req: Request }) => {
+  return createInnerTRPCContext({
+    headers: opts.req.headers,
+  });
+};
+
+/**
+ * 2. INITIALIZATION
+ *
+ * This is where the trpc api is initialized, connecting the context and
+ * transformer
+ */
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+/**
+ * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
+ *
+ * These are the pieces you use to build your tRPC API. You should import these
+ * a lot in the /src/server/api/routers folder
+ */
+
+/**
+ * This is how you create new routers and subrouters in your tRPC API
+ * @see https://trpc.io/docs/router
+ */
+export const createTRPCRouter = t.router;
+
+/**
+ * Public (unauthed) procedure
+ *
+ * This is the base piece you use to build new queries and mutations on your
+ * tRPC API. It does not guarantee that a user querying is authorized, but you
+ * can still access user session data if they are logged in
+ */
+export const publicProcedure = t.procedure;
+
+/**
+ * Reusable middleware that enforces users are logged in before running the
+ * procedure
+ */
+const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+    },
+  });
+});
+
+/**
+ * Protected (authed) procedure
+ *
+ * If you want a query or mutation to ONLY be accessible to logged in users, use
+ * this. It verifies the session is valid and guarantees ctx.session.user is not
+ * null
+ *
+ * @see https://trpc.io/docs/procedures
+ */
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
