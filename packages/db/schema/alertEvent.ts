acd44189419d7b06f9e8268cb17f3afd642d56e8
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-14 14:37:20 -0500

    Update schema

--- packages/db/schema/alertEvent.ts
+++ packages/db/schema/alertEvent.ts
@@ -1,8 +1,8 @@
-import { foreignKey, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 import { alert } from './alert';
 
-export const alertEvent = pgTable('alert-event', {
+export const alertEvent = pgTable('alert_event', {
   id: text('id').primaryKey(), // v_alert-event_[hash]
   alertId: text('id').references(() => alert.id),
   createdAt: timestamp('created_at').notNull(),
