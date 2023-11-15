53969a65720d0ac04431e9f20a9aaed2c9b72b86
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-15 11:38:35 -0500

    Convert aws to make

--- apps/backend/src/routes/webhook.ts
+++ apps/backend/src/routes/webhook.ts
@@ -1,11 +1,12 @@
 import { APIGatewayEvent } from 'aws-lambda';
 
-import { publish, Topic } from '@vessel/api/src/libs/aws';
+import { makeAws } from '@vessel/api/src/libs/aws';
 
 export async function main(event: APIGatewayEvent) {
   console.log({ body: event.body });
-  await publish({
-    topic: Topic.ALERT,
+  const { sqs } = makeAws();
+  await sqs.publish({
+    topic: sqs.TOPIC.ALERT,
     payload: { test: 1 },
   });
   return { statusCode: 200, body: { success: false } };
--- packages/api/src/libs/aws.ts
+++ packages/api/src/libs/aws.ts
@@ -2,26 +2,35 @@ import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 
 import { env } from '../../env.mjs';
 
-const client = new SNSClient();
+const makeSqs = () => {
+  const client = new SNSClient();
+  const TOPIC = {
+    ALERT: env.AWS_SNS_TOPIC_ALERT_ARN,
+  } as const;
+  type TOPIC = (typeof TOPIC)[keyof typeof TOPIC];
 
-export const Topic = {
-  ALERT: env.AWS_SNS_TOPIC_ALERT_ARN,
-} as const;
-type Topic = (typeof Topic)[keyof typeof Topic];
+  const publish = async ({
+    topic,
+    payload,
+  }: {
+    topic: TOPIC;
+    payload: object;
+  }) => {
+    const params = new PublishCommand({
+      TopicArn: topic,
+      Message: JSON.stringify(payload),
+    });
+    await client.send(params);
+  };
 
-const publish = async ({
-  topic,
-  payload,
-}: {
-  topic: Topic;
-  payload: object;
-}) => {
-  const params = new PublishCommand({
-    TopicArn: topic,
-    Message: JSON.stringify(payload),
-  });
-  const test = await client.send(params);
-  console.log({ test });
+  return {
+    TOPIC,
+    publish,
+  };
 };
 
-export { publish };
+export const makeAws = () => {
+  return {
+    sqs: makeSqs(),
+  };
+};
