27a088f4afc3fc06c4636b0447c039427d162cfb
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-16 14:13:53 -0500

    Update secrets

--- packages/db/schema/secret.ts
+++ packages/db/schema/secret.ts
@@ -2,7 +2,7 @@ import { sql } from 'drizzle-orm';
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const secret = pgTable('secret', {
-  id: text('id').primaryKey(), // v_organization_[hash]
+  id: text('id').primaryKey(),
   iv: text('iv').notNull(),
   encryptedData: text('encrypted_data').notNull(),
   createdAt: timestamp('created_at')
