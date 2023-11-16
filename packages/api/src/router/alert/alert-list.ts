8d24d9602d3630183d35ab93876c5857f4ac3933
zkirby <zkirby16@gmail.com>
2023-11-16 13:06:35 -0500

    fix typing on filtering and sorting clauses

--- packages/api/src/router/alert/alert-list.ts
+++ packages/api/src/router/alert/alert-list.ts
@@ -1,7 +1,7 @@
-import { input, z } from 'zod';
+import { z } from 'zod';
 
-import type { Db } from '@vessel/db';
-import { asc, db, desc, schema } from '@vessel/db';
+import type { Db, SQL } from '@vessel/db';
+import { and, asc, db, desc, eq, ilike, not, or, schema } from '@vessel/db';
 
 import { useContextHook } from '../../middlewares/use-context-hook';
 import { useLogger } from '../../middlewares/use-logger';
@@ -11,51 +11,100 @@ interface Context {
   db: Db;
 }
 
+// NOTE(@zkirby): Order matters, preceding sorts will sort first.
+const sorts = z
+  .array(
+    z.object({
+      property: z.enum(['status', 'title', 'assignedToId', 'createdAt']),
+      order: z.enum(['asc', 'desc']).optional().default('asc'),
+    }),
+  )
+  .optional();
+
 const Conditions = {
   Is: 'IS',
   IsNot: 'IS_NOT',
   Contains: 'CONTAINS',
 };
 
-
-// NOTE(@zkirby): Order matters as preceding sorts will sort the list first.
-const sorts = z
-.array(
-  z.object({
-    property: z.enum(['status', 'title', 'assignedToId', 'createdAt']),
-    order: z.enum(['asc', 'desc']).optional().default('asc'),
-  }),
-)
-.optional()
-
 // NOTE(@zkirby): All conditions are AND-ed, ORs can be done by supplying
-// multiple values in the same filter (e.g., `value: ['ACKED', 'CLOSED']`)
-const filters =  z.array(
-  z.object({
-  status: z.object({
-    value: z.array(z.enum(['ACKED', 'OPEN', 'CLOSED'])),
-    condition: z.enum([Conditions.Is, Conditions.IsNot]),
-  }).or(z.object({
-    title: z.object({
-      value: z.string(),
-      // Supports the search functionality on the FE
-      // for now until we implement more efficient search.
-      condition: z.enum([Conditions.Contains]),
-    })
-  })).or(
-    z.object({
-      assignedToId: z.object({
-        value: z.string(),
-        // Only support "is assigned to" for now.
-        condition: z.enum([Conditions.Is]),
+// multiple values in the same filter (e.g., `value: ['ACKED', 'CLOSED']`).
+// conditions should always take the shape:
+// {
+//    [property name]: {
+//        value: z.any(),
+//        conditions: z.enum(Conditions)
+//    }
+// }
+const filters = z
+  .array(
+    z
+      .object({
+        status: z.object({
+          value: z.array(z.enum(['ACKED', 'OPEN', 'CLOSED'])),
+          condition: z.enum([Conditions.Is, Conditions.IsNot]),
+        }),
       })
-    })
-  ),
-  // TODO(@zkirby): Add filter support for createdAt times.
-})
-.partial()
-.optional()  
-)
+      .or(
+        z.object({
+          title: z.object({
+            value: z.string(),
+            // Supports the search functionality on the FE
+            // for now until we implement more efficient search.
+            condition: z.enum([Conditions.Contains]),
+          }),
+        }),
+      )
+      .or(
+        z.object({
+          assignedToId: z.object({
+            value: z.string(),
+            // Only support "is assigned to" for now.
+            condition: z.enum([Conditions.Is]),
+          }),
+        }),
+      ),
+    // TODO(@zkirby): Add filter support for createdAt times.
+  )
+  .optional();
+
+const buildSortClause = (inputSorts: z.infer<typeof sorts>) => {
+  if (!inputSorts?.length) return [];
+  return inputSorts.map((s) => {
+    const orderFn = s.order === 'asc' ? asc : desc;
+    return orderFn(schema.alert[s.property]);
+  });
+};
+
+const buildFilterClause = (inputFilters: z.infer<typeof filters>) => {
+  if (!inputFilters?.length) return undefined;
+
+  return inputFilters.reduce<SQL<unknown> | undefined>((curr, filter) => {
+    if ('status' in filter) {
+      const statusEquals = filter.status.value.map((v) =>
+        eq(schema.alert.status, v),
+      );
+
+      // expands to something like: not(or(eq(col, 'ACKED'), eq(col, 'CLOSED')))
+      const conditions = or(...statusEquals)!;
+      const shouldBeInverted = filter.status.condition === Conditions.IsNot;
+      const statusCondition = shouldBeInverted ? not(conditions) : conditions;
+
+      return and(statusCondition, curr);
+    } else if ('title' in filter) {
+      // WARNING(@zkirby): Absolutely not the most efficient way to implement this
+      // filter but should be fine for now until we need to do something more efficient.
+      const titleIsLike = ilike(schema.alert.title, `%${filter.title.value}%`);
+      return and(titleIsLike, curr);
+    } else if ('assignedToId' in filter) {
+      const assignedToIdIs = eq(
+        schema.alert.assignedToId,
+        filter.assignedToId.value,
+      );
+      return and(assignedToIdIs, curr);
+    }
+  }, undefined);
+};
 
 const input = z.object({
   limit: z.number().optional(),
@@ -63,29 +112,6 @@ const input = z.object({
   sorts,
   filters,
 });
-
-
-const buildSortClause = (inputSorts: z.infer<typeof sorts>) => {
-  if (!inputSorts?.length) return [];
-  return inputSorts.map(s => {
-    const orderFn = s.order === 'asc' ? asc : desc
-    return orderFn(schema.alert[s.property]);
-  })
-}
-
-// const buildFilterClause = (inputFilters: z.infer<typeof filters>) => {
-//   if (!inputFilters) return null;
-
-//   const filterClause = {}
-//   // if (inputFilters?.status) {
-//   //   filterClause = {
-//   //     ...filterClause,
-//   //     status: 
-//   //   }
-//   // }
-
-//   return filterClause;
-// };
 
 export const alertList = publicProcedure
   .use(
@@ -97,12 +123,18 @@ export const alertList = publicProcedure
   .input(input)
   .query(({ ctx, input }) => {
     const sortClause = buildSortClause(input.sorts);
-    // const filterClause = buildFilterClause(input.filters);
+    const filterClause = buildFilterClause(input.filters);
 
     return ctx.db.query.alert.findMany({
       limit: input.limit,
       offset: input.offset,
       orderBy: sortClause,
-      // where: filterClause,
+      where: filterClause,
     });
   });
+
+/**
+ * TODO:
+ * - implement unit tests
+ * - add id validations with drizzle-zod
+ */
--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,44 +1,45 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: [
-    "turbo",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "prettier",
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
   ],
   env: {
     es2022: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "turbo/no-undeclared-env-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    'turbo/no-undeclared-env-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports", fixStyle: "separate-type-imports" },
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
     ],
-    "@typescript-eslint/no-misused-promises": [
+    '@typescript-eslint/no-misused-promises': [
       2,
       { checksVoidReturn: { attributes: false } },
     ],
-    "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   ignorePatterns: [
-    "**/.eslintrc.cjs",
-    "**/*.config.js",
-    "**/*.config.cjs",
-    ".next",
-    "dist",
-    "pnpm-lock.yaml",
+    '**/.eslintrc.cjs',
+    '**/*.config.js',
+    '**/*.config.cjs',
+    '.next',
+    'dist',
+    'pnpm-lock.yaml',
   ],
   reportUnusedDisableDirectives: true,
 };
