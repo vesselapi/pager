66454c13f99d462600698739237fe269ef1f322e
zkirby <zkirby16@gmail.com>
2023-11-20 10:16:27 -0800

    add strict

--- packages/api/src/router/alert/alert-list.ts
+++ packages/api/src/router/alert/alert-list.ts
@@ -4,7 +4,6 @@ import type { Db, SQL } from '@vessel/db';
 import { and, asc, db, desc, eq, ilike, not, or, schema } from '@vessel/db';
 import { UserIdRegex } from '@vessel/types';
 
-import { useLogger } from '../../middlewares/use-logger';
 import { useServicesHook } from '../../middlewares/use-services-hook';
 import { publicProcedure } from '../../trpc';
 
@@ -15,10 +14,12 @@ interface Context {
 // NOTE(@zkirby): Order matters, preceding sorts will sort first.
 const sorts = z
   .array(
-    z.object({
-      property: z.enum(['status', 'title', 'assignedToId', 'createdAt']),
-      order: z.enum(['asc', 'desc']).optional().default('asc'),
-    }),
+    z
+      .object({
+        property: z.enum(['status', 'title', 'assignedToId', 'createdAt']),
+        order: z.enum(['asc', 'desc']).optional().default('asc'),
+      })
+      .strict(),
   )
   .optional();
 
@@ -44,24 +45,29 @@ const filters = z
         value: z.array(z.enum(['ACKED', 'OPEN', 'CLOSED'])),
         condition: z.enum([Conditions.Is, Conditions.IsNot]),
       })
+      .strict()
       .or(
-        z.object({
-          property: z.literal('title'),
-          value: z.string(),
-          // Supports the search functionality on the FE
-          // for now until we implement more efficient search.
-          condition: z.enum([Conditions.Contains]),
-        }),
+        z
+          .object({
+            property: z.literal('title'),
+            value: z.string(),
+            // Supports the search functionality on the FE
+            // for now until we implement more efficient search.
+            condition: z.enum([Conditions.Contains]),
+          })
+          .strict(),
       )
       .or(
-        z.object({
-          property: z.literal('assignedToId'),
-          value: z
-            .string()
-            .regex(UserIdRegex, `Invalid id, expected format ${UserIdRegex}`),
-          // Only support "is assigned to" for now.
-          condition: z.enum([Conditions.Is]),
-        }),
+        z
+          .object({
+            property: z.literal('assignedToId'),
+            value: z
+              .string()
+              .regex(UserIdRegex, `Invalid id, expected format ${UserIdRegex}`),
+            // Only support "is assigned to" for now.
+            condition: z.enum([Conditions.Is]),
+          })
+          .strict(),
       ),
     // TODO(@zkirby): Add filter support for createdAt times.
   )
@@ -102,12 +108,14 @@ const buildFilterClause = (inputFilters: z.infer<typeof filters>) => {
   }, undefined);
 };
 
-const input = z.object({
-  limit: z.number().optional(),
-  offset: z.number().optional(),
-  sorts,
-  filters,
-});
+const input = z
+  .object({
+    limit: z.number().optional(),
+    offset: z.number().optional(),
+    sorts,
+    filters,
+  })
+  .strict();
 
 export const alertList = publicProcedure
   .use(
@@ -115,7 +123,6 @@ export const alertList = publicProcedure
       db: () => db,
     }),
   )
-  .use(useLogger())
   .input(input)
   .query(({ ctx, input }) => {
     const sortClause = buildSortClause(input.sorts);
