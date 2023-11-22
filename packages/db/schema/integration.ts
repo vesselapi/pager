64e555c3b8e272bfe5594641aafd2247e9350680
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-11-22 10:47:03 -0800

    Rename

--- packages/db/schema/integration.ts
+++ packages/db/schema/integration.ts
@@ -3,13 +3,13 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 
 import { APP_ID, IntegrationIdRegex } from '@vessel/types';
-import type { AppId, IntegrationId, OrgId } from '@vessel/types';
+import type { IntegrationId, OrgId } from '@vessel/types';
 
 import { organization } from './organization';
 
 export const integration = pgTable('integration', {
   id: text('id').primaryKey(), // v_integration_[hash]
-  organizationId: text('organization_id')
+  orgId: text('org_id')
     .references(() => organization.id)
     .notNull(),
   appId: text('app_id', { enum: APP_ID }).notNull(),
@@ -18,8 +18,7 @@ export const integration = pgTable('integration', {
 
 export const selectIntegrationSchema = createSelectSchema(integration, {
   id: (schema) => schema.id.transform((x) => x as IntegrationId),
-  organizationId: (schema) =>
-    schema.organizationId.transform((x) => x as OrgId),
+  orgId: (schema) => schema.orgId.transform((x) => x as OrgId),
 });
 
 export const insertIntegrationSchema = createInsertSchema(integration, {
@@ -30,8 +29,7 @@ export const insertIntegrationSchema = createInsertSchema(integration, {
         `Invalid id, expected format ${IntegrationIdRegex}`,
       )
       .transform((x) => x as IntegrationId),
-  organizationId: (schema) =>
-    schema.organizationId.transform((x) => x as OrgId),
+  orgId: (schema) => schema.orgId.transform((x) => x as OrgId),
 });
 
 export type CreateIntegration = z.infer<typeof insertIntegrationSchema>;
