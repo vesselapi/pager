d52dd3572d44ee84fed84b51f7e5e2172a70bd47
zkirby <zkirby16@gmail.com>
2023-12-04 20:04:42 -0800

    remove left over drizzle folder changes from bad migration

--- packages/db/drizzle/schema.ts
+++
@@ -1,7 +0,0 @@
-export const alertSource = pgEnum('alert_source', ['vessel', 'sentry']);
-export const status = pgEnum('status', ['CLOSED', 'OPEN', 'ACKED']);
-export const appId = pgEnum('app_id', ['sentry']);
-export const escalationPolicyStepType = pgEnum('escalation_policy_step_type', [
-  'ROTATION',
-  'USER',
-]);
