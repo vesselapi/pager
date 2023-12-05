405a3d6ad8ac26cc693766959d8b5ed5d82c6e57
zkirby <zkirby16@gmail.com>
2023-12-04 17:09:33 -0800

    add profile pic to icon

--- apps/nextjs/src/app/_components/UserIcon.tsx
+++ apps/nextjs/src/app/_components/UserIcon.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import Image from 'next/image';
 
 const createInitials = (
   firstName?: string | null,
@@ -9,17 +10,17 @@ const createInitials = (
 };
 
 const UserIcon = ({
-  size = 'sm',
   className,
   firstName,
   lastName,
+  imageUrl,
 }: {
-  size?: 'sm' | 'md' | 'lg';
   className?: string;
   firstName?: string | null;
   lastName?: string | null;
+  imageUrl?: string | null;
 }) => {
-  if (!firstName && !lastName) {
+  if (!firstName && !lastName && !imageUrl) {
     return (
       <div
         className={classNames(
@@ -28,17 +29,22 @@ const UserIcon = ({
         )}
       ></div>
     );
+  } else if (imageUrl) {
+    return (
+      <Image
+        className="flex items-center justify-center rounded-full bg-slate-500 text-slate-200 h-[20px] w-[20px] text-xxs object-cover border-2 border-white"
+        src={imageUrl}
+        alt={'user profile picture'}
+        width={20}
+        height={20}
+      />
+    );
   }
   return (
     <div
       className={classNames(
         className,
-        'flex items-center justify-center rounded-full ring-2 ring-white bg-slate-500 text-slate-200',
-        {
-          'h-[20px] w-[20px] text-xxs': size === 'sm',
-          'h-[30px] w-[30px] text-xs': size === 'md',
-          'h-[40px] w-[40px] text-sm': size === 'lg',
-        },
+        'flex items-center justify-center rounded-full ring-2 ring-white bg-slate-500 text-slate-200 h-[20px] w-[20px] text-xxs',
       )}
     >
       <div>{createInitials(firstName, lastName)}</div>
--- packages/api/src/router/user/index.ts
+++ packages/api/src/router/user/index.ts
@@ -1,8 +1,8 @@
 import { createTRPCRouter } from '../../trpc';
+import { userList } from './list';
 import { userMe } from './me';
 import { userUpdate } from './update';
 import { userUpdateExpoPushToken } from './update-expo-push-token';
-import { userList } from './user-list';
 
 export const userRouter = createTRPCRouter({
   list: userList,
