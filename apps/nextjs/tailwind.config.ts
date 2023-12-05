9e6a952030b785da5ec5c0c424f36e00d57cd8c5
zkirby <zkirby16@gmail.com>
2023-12-04 20:00:58 -0800

    fix tailwind config

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   content: ['./src/**/*.{ts,tsx}'],
   presets: [baseConfig],
   safelist: [
-    // Used by the Calendar component grid-cols 7-14
+    // Used by the Calendar component
     ...Array.from({ length: 15 }, (_, i) => `grid-cols-${i + 1}`),
     ...Array.from({ length: 15 }, (_, i) => `col-span-${i + 1}`),
   ],
@@ -24,23 +24,6 @@ export default {
       },
       colors: {
         'light-grey': '#fafafa',
-
-        'red-500': '#F15D3D',
-        'red-400': '#F37155',
-        'red-300': '#F4856E',
-        'red-200': '#F69A86',
-        'red-100': '#F8AE9E',
-
-        'green-500': '#45E93A',
-        'green-400': '#5CEC53',
-        'green-300': '#74EE6B',
-        'green-200': '#8BF184',
-        'green-100': '#A2F49D',
-
-        'purple-400': '#C454F3',
-        'purple-300': '#CD6DF4',
-        'purple-200': '#D585F6',
-        'purple-100': '#DE9DF8',
       },
       /**
        * @Guidelines - Lean smaller, more compact sizes.
